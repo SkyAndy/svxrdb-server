e8ee87db1b9aee852db96bd38bce06861a92cce0
SkyAndy <skyandy.am@gmail.com>
2019-12-23 05:20:21 +0100

    cleaning code

--- tgdb.php
+++ tgdb.php
@@ -1,11 +1,11 @@
 <?php
 /* talkgroup / number alias database */
 $tgdb_array = [
-    '0'     => 'IDLE',
-    '262'    => 'Deutschland',
-    '2621'     => 'Berlin Brandenburg',
-    '2629'     => 'Thueringen', /* ATTENTION last entry have no , at end of line !!! */
-    '2621053' => 'DO7EN PC'
+    '0'     	=> 'IDLE',
+    '262'	=> 'Deutschland',
+    '2621'	=> 'Berlin Brandenburg',
+    '2629'	=> 'Thueringen', /* ATTENTION last entry have no , at end of line !!! */
+    '2621053'	=> 'DO7EN PC'
 ];
 
 ?>
