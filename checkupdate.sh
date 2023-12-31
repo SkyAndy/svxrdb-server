03a28694b5bf3f1f4914f66cb9d2b43ec517ee65
SkyAndy <skyandy.am@gmail.com>
2023-12-31 12:24:18 +0100

    Update checkupdate.sh

--- checkupdate.sh
+++ checkupdate.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-# DO7EN 22.12.2020
+# DK1LO 31.12.2024
 # Please create this script as a cron job for the user svxlink.
 # The path indicates where the config from the dashboard is located.
 #
 # su svxlink
 # crontab -e
-# 8 * * * * nice /home/svxlink/html/svxrdb/checkupdate.sh >/dev/null 2>&1
+# 8 * * * * nice /var/www/html/svxrdb-server/checkupdate.sh >/dev/null 2>&1
 
-DIRECTORY=/home/svxlink/html/svxrdb
+WWWDIRECTORY=/var/www/html/svxrdb-server
 SVXL_VERSION=$(wget -qO- https://raw.githubusercontent.com/sm0svx/svxlink/master/src/versions | grep "SVXLINK=" | cut -d"=" -f2)
 # attention !!! -i write direct to file..remove -i for testing
-sed -i 's/SVXLINKVERSION.*$/SVXLINKVERSION\"\,\ \"'$SVXL_VERSION'\"\ \)\;/' $DIRECTORY/config.php
+sed -i 's/SVXLINKVERSION.*$/SVXLINKVERSION\"\,\ \"'$SVXL_VERSION'\"\ \)\;/' $WWWDIRECTORY/config.php
 
 # add your own source directory
 SRC_DIRECTORY=/home/svxlink/svxlink/src
 SVXR_VERSION=$(wget -qO- https://raw.githubusercontent.com/sm0svx/svxlink/master/src/versions | grep "SVXREFLECTOR=" | cut -d"=" -f2)
 SVXR_VERSION_LOCAL=$(grep SVXREFLECTOR $SRC_DIRECTORY/versions | cut -d "=" -f2)
 # attention !!! -i write direct to file..remove -i for testing
-sed -i 's/SVXREFLECTORVERSION.*$/SVXREFLECTORVERSION\"\,\ \"'$SVXR_VERSION'\ -> '$SVXR_VERSION_LOCAL'\"\ \)\;/' $DIRECTORY/config.php
-
+sed -i 's/SVXREFLECTORVERSION.*$/SVXREFLECTORVERSION\"\,\ \"'$SVXR_VERSION'\ -> '$SVXR_VERSION_LOCAL'\"\ \)\;/' $WWWDIRECTORY/config.php
